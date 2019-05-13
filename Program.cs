90bf0b7a4db558c076cd687c4ae2d6174552c797
Tran Minh Hieu <tranhieu23.code@outlook.com>
2019-05-13 16:21:58 +0700

    Finished simulation

--- Program.cs
+++ Program.cs
@@ -34,8 +34,7 @@ class Program
     public static void Main(string[] args)
     {
         Building building = Building.LoadFromFile("data.bld");
-        Console.WriteLine(building.Floors.Count);
-        Console.WriteLine(building.Exits.Count);
-        Console.WriteLine(building.Inhabitants.Count);
+        Simulator simulator = new Simulator(building, new MyAlgorithm(), new MyHazard());
+        double result = simulator.RunSimulator(2000, 10000);
     }
 }
