4014f0ef29dc2dbb019dfa669528f1713ea1c95a
Tran Minh Hieu <tranhieu23.code@outlook.com>
2019-05-15 14:16:11 +0700

    Make changes to input file format

--- EvaFrame/Models/Building/Building.cs
+++ EvaFrame/Models/Building/Building.cs
@@ -45,7 +45,8 @@ namespace EvaFrame.Models.Building
                 {
                     int numFloor = Int32.Parse(sr.ReadLine());
                     for (int floorId = 0; floorId < numFloor; floorId++)
-                        LoadFloor(sr, result, floorId);
+                        LoadFloor(sr, result);
+                    LoadExitNodes(sr, result);
                     LoadPeople(sr, result);
                 }
                 return result;
@@ -64,97 +65,128 @@ namespace EvaFrame.Models.Building
         /// <param name="updatePeriod">Khoảng thời gian di chuyển.</param>
         public void MoveInhabitants(double updatePeriod)
         {
-            for (int i = 0; i < inhabitants.Count; i ++)
+            for (int i = 0; i < inhabitants.Count; i++)
             {
                 Person p = inhabitants[i];
                 if (p.Evacuate(updatePeriod))
                 {
                     inhabitants.RemoveAt(i);
-                    i --;
+                    i--;
                 }
             }
         }
 
-        private static void LoadFloor(StreamReader sr, Building target, int floorId)
+        private static void LoadFloor(StreamReader sr, Building target)
+        {
+            List<Indicator> indicatorList = LoadIndicatorsInFloor(sr);
+            LoadCorridorsInFloor(sr, indicatorList);
+            List<Indicator> stairList = LoadStairNodesInFloor(sr, indicatorList);
+            LoadStairCorridorsInFloor(sr, target, indicatorList);
+            target.floors.Add(new Floor(indicatorList, stairList));
+        }
+
+        private static List<Indicator> LoadIndicatorsInFloor(StreamReader sr)
         {
             List<Indicator> indicatorList = new List<Indicator>();
             int numInd = Int32.Parse(sr.ReadLine());
+            if (numInd == 0)
+                return indicatorList;
             sr.ReadLine(); // TODO: This line contains the coordinates of the Indicators, and can be used in graphic representation
             for (int i = 0; i < numInd; i++)
                 indicatorList.Add(new Indicator());
+            return indicatorList;
+        }
 
+        private static void LoadCorridorsInFloor(StreamReader sr, List<Indicator> indicatorList)
+        {
             int numCor = Int32.Parse(sr.ReadLine());
+            if (numCor == 0)
+                return;
             string[] corridorData = sr.ReadLine().Split(',');
             foreach (string data in corridorData)
             {
                 string[] values = data.Split(';');
-                int fromId = Int32.Parse(values[0]);
-                Indicator from = indicatorList[fromId - 1];
-                int toId = Int32.Parse(values[1]);
-                Indicator to = indicatorList[toId - 1];
+                Indicator from = IdToIndicator(indicatorList, values[0]);
+                Indicator to = IdToIndicator(indicatorList, values[1]);
                 double length = Double.Parse(values[2]);
                 double width = Double.Parse(values[3]);
                 double trustiness = Double.Parse(values[4]);
-
                 from.Neighbors.Add(new Corridor(from, to, length, width, 0, trustiness));
             }
+        }
 
+        private static List<Indicator> LoadStairNodesInFloor(StreamReader sr, List<Indicator> indicatorList)
+        {
             List<Indicator> stairList = new List<Indicator>();
             int numStair = Int32.Parse(sr.ReadLine());
+            if (numStair == 0)
+                return stairList;
             string[] stairNodeIds = sr.ReadLine().Split(',');
             foreach (string idString in stairNodeIds)
             {
-                int id = Int32.Parse(idString) - 1;
-                indicatorList[id].IsStairNode = true;
-                stairList.Add(indicatorList[id]);
+                Indicator stair = IdToIndicator(indicatorList, idString);
+                stair.IsStairNode = true;
+                stairList.Add(stair);
             }
+            return stairList;
+        }
 
-            if (floorId == 0)
+        private static void LoadStairCorridorsInFloor(StreamReader sr, Building target, List<Indicator> indicatorList)
+        {
+            int numStairCor = Int32.Parse(sr.ReadLine());
+            if (numStairCor == 0)
+                return;
+            string[] stairData = sr.ReadLine().Split(',');
+            foreach (string data in stairData)
             {
-                int numExit = Int32.Parse(sr.ReadLine());
-                string[] exitNodeIds = sr.ReadLine().Split(',');
-                foreach (string idString in exitNodeIds)
-                {
-                    int id = Int32.Parse(idString) - 1;
-                    indicatorList[id].IsExitNode = true;
-                    target.exits.Add(indicatorList[id]);
-                }
+                string[] values = data.Split(';');
+                Indicator from = IdToIndicator(indicatorList, values[0]);
+                Indicator to = IdToIndicator(target, values[1]);
+                double length = Double.Parse(values[2]);
+                double width = Double.Parse(values[3]);
+                double trustiness = Double.Parse(values[4]);
+                from.Neighbors.Add(new Corridor(from, to, length, width, 0, trustiness));
+                to.Neighbors.Add(new Corridor(to, from, length, width, 0, trustiness));
             }
-            else
-            {
-                int numStairCor = Int32.Parse(sr.ReadLine());
-                string[] stairData = sr.ReadLine().Split(',');
-                foreach (string data in stairData)
-                {
-                    string[] values = data.Split(';');
-                    int fromId = Int32.Parse(values[0]);
-                    Indicator from = indicatorList[fromId - 1];
-                    int toId = Int32.Parse(values[1]);
-                    Indicator to = target.floors[floorId - 1].Indicators[toId - 1];
-                    double length = Double.Parse(values[2]);
-                    double width = Double.Parse(values[3]);
-                    double trustiness = Double.Parse(values[4]);
+        }
 
-                    from.Neighbors.Add(new Corridor(from, to, length, width, 0, trustiness));
-                    to.Neighbors.Add(new Corridor(to, from, length, width, 0, trustiness));
-                }
+        private static void LoadExitNodes(StreamReader sr, Building target)
+        {
+            int numExit = Int32.Parse(sr.ReadLine());
+            string[] exitNodeIds = sr.ReadLine().Split(',');
+            foreach (string idString in exitNodeIds)
+            {
+                Indicator exit = IdToIndicator(target, idString);
+                exit.IsExitNode = true;
+                target.exits.Add(exit);
             }
-            target.floors.Add(new Floor(indicatorList, stairList));
         }
 
         private static void LoadPeople(StreamReader sr, Building target)
         {
             int numPeople = Int32.Parse(sr.ReadLine());
-            for (int i = 0; i < numPeople; i ++)
+            for (int i = 0; i < numPeople; i++)
             {
                 string[] peopleData = sr.ReadLine().Split(';');
-                int floorId = Int32.Parse(peopleData[0]);
+                Indicator following = IdToIndicator(target, peopleData[0]);
                 double speedMax = Double.Parse(peopleData[1]);
-                int followingId = Int32.Parse(peopleData[4]);
-                Indicator following = target.floors[floorId - 1].Indicators[followingId - 1];
-
                 target.inhabitants.Add(new Person(speedMax, following));
             }
+        }
+
+        private static Indicator IdToIndicator(Building target, string idString)
+        {
+            string[] indicatorData = idString.Split('@');
+            int indicatorId = Int32.Parse(indicatorData[0]);
+            int floorId = Int32.Parse(indicatorData[1]);
+            return target.floors[floorId - 1].Indicators[indicatorId - 1];
+        }
+
+        private static Indicator IdToIndicator(List<Indicator> indicatorList, string idString)
+        {
+            string[] indicatorData = idString.Split('@');
+            int fromId = Int32.Parse(indicatorData[0]);
+            return indicatorList[fromId - 1];
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Building building = Building.LoadFromFile("data_prime.bld");
+        Building building = Building.LoadFromFile("data.bld");
         Simulator simulator = new Simulator(building, new PlainDijikstra(), new BasicConstantHazard());
         double result = simulator.RunSimulator(2000, 10000);
     }
