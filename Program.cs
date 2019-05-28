749d209b2476c07430d3fb522274718d333afaf1
Tran Minh Hieu <tranhieu23.code@outlook.com>
2019-05-28 22:03:23 +0700

    Finished README.md

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Threading;
 using Avalonia;
 using Avalonia.Logging.Serilog;
 using EvaFrame.Models.Building;
-using EvaFrame.Algorithm.PlainDijikstra;
+using EvaFrame.Algorithm;
 using EvaFrame.Algorithm.PFAlgorithm;
 using EvaFrame.Simulator;
 using EvaFrame.Simulator.Hazards;
 using EvaFrame.Visualization.WindowVisualization;
-using EvaFrame.Algorithm.LCDTAlgorithm;
-
 
 class Program
 {
@@ -24,8 +21,10 @@ class Program
     private static void AppMain(Application app, string[] args)
     {
         Building target = Building.LoadFromFile("data.bld");
+        IAlgorithm algorithm = new PFAlgorithm(4, 40);
+        IHazard hazard = new RandomNonCriticalHazard(new int[] {5, 6, 7, 8}, 0.3, 3);
         WindowVisualization visualization = new WindowVisualization();
-        Simulator simulator = new Simulator(target, new PFAlgorithm(4, 40), new RandomNonCriticalHazard(new int[] {5, 6, 7, 8}, 0.3, 3), visualization);
+        Simulator simulator = new Simulator(target, algorithm, hazard, visualization);
         simulator.RunSimulatorAsync(0.1, 10, "SimulationData.csv");
         app.Run(visualization.MainWindow);
     }
